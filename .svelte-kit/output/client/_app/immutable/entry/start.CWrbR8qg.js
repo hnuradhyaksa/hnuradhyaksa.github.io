bdfd87ef48a83a81aaefd4560b53618a7c00a715
hnuradhyaksa <hnuradhyaksa@gmail.com>
2025-02-02 09:35:24 +0700

    Add responsiveness main page

---
+++ .svelte-kit/output/client/_app/immutable/entry/start.CWrbR8qg.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/Cqu7jr97.js";export{t as start};
---
+++ .svelte-kit/output/server/entries/pages/post/coach-nim-shin/_page.svelte.js
@@ -0,0 +1,14 @@
+import "clsx";
+import { c as pop, p as push } from "../../../../chunks/index.js";
+import "d3";
+function Coaching_path($$payload, $$props) {
+  push();
+  $$payload.out += `<svg class="svelte-ida2qq"></svg>`;
+  pop();
+}
+function _page($$payload) {
+  Coaching_path($$payload);
+}
+export {
+  _page as default
+};
